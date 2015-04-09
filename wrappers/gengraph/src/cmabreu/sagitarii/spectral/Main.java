7dae1823d1220dd712a99669961cf91504b126b9
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2015-04-08 23:51:33 -0300

    Criação da tarefa de segmentação de matrizes (AWK).

--- wrappers/gengraph/src/cmabreu/sagitarii/spectral/Main.java
+++ wrappers/gengraph/src/cmabreu/sagitarii/spectral/Main.java
@@ -1,6 +1,7 @@
 package cmabreu.sagitarii.spectral;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -21,34 +22,54 @@ public class Main {
 		// maxdegree,biptonly,optifunc,mindegree,trianglefree,laplacian,allowdiscgraphs,ordermin,caixa1,slaplacian,adjacency,ordermax
 		// 0         1        2        3         4            5         6               7        8      9          10        11       
 		
-		String maxDegree = lineData[0];
-		String biptOnly = lineData[1];
 		String optiFunc = lineData[2];
-		String minDegree = lineData[3];
-		String triangleFree = lineData[4];
 		String laplacian = lineData[5];
-		String allowDiscGraphs = lineData[6];
-		String ordermin = lineData[7];
+		String adjacency = lineData[10];
 		String caixa1 = lineData[8];
 		String slaplacian = lineData[9];
-		String adjacency = lineData[10];
-		String ordermax = lineData[11];
+
 		
-		generateParameter(adjacency, laplacian, slaplacian, optiFunc, caixa1, ordermin, ordermax, minDegree, maxDegree, triangleFree, allowDiscGraphs, biptOnly);
+		String maxDegree = lineData[0];
+		String biptOnly = lineData[1];
+		String minDegree = lineData[3];
+		String triangleFree = lineData[4];
+		String allowDiscGraphs = lineData[6];
+		String ordermin = lineData[7];
+		String ordermax = lineData[11];
 		
 		String libraryDirectory = readLibraryDirectory();
 		if( !libraryDirectory.equals("")  ) {
 			
-			String gengOutput = workFolder + "/saida.g6";
-			String showgOutput = workFolder + "/saida.txt";
+			String degreeOptions = " -d" + minDegree + " -D" + maxDegree;
+			String gengOptions = "";
+			if ( biptOnly.equals("on") ) {
+				gengOptions += "-b ";
+			}
+			if ( triangleFree.equals("on") ) {
+				gengOptions += "-t ";
+			}			
+			if ( !allowDiscGraphs.equals("on") ) {
+				gengOptions += "-c ";
+			}			
 			
-			String geng = libraryDirectory + "/geng -g -q 4 " + gengOutput;
-			String showg = libraryDirectory + "/showg -q -A " + gengOutput + " " + showgOutput;
+
+			String order = "10"; // Order deverá vir do Sagitarii ( uma tupla para cada valor entre minorder e maxorder )
+		
+			String gengOutput = workFolder + "/saida_" + order +  ".g6";
+			String showgOutput = workFolder + "/saida_" + order +  ".txt";
+
+			System.out.println( gengOutput );
 			
+			
+			String geng = libraryDirectory + "/geng " + degreeOptions + " -g -q " + order + " " + gengOptions + " " + gengOutput;
 			run(geng);
+
+			String showg = libraryDirectory + "/showg -a " + gengOutput + " " + showgOutput;
 			run(showg);
-			
-			// awk '/^Graph/{close("graph_"f);f++}{print $0 > "graph_"f}' x.converted
+
+			String awk = "awk '/^Graph/{close(\"graph_\"f);f++}{print $0 > \"graph_\"f}' " + showgOutput;
+			runSystem( awk, workFolder );
+
 			
 		} else {
 			System.out.println("Cannot find config file spectral.config");
@@ -90,8 +111,35 @@ public class Main {
 	}	
 	
 	
+
+	private static int runSystem( String command, String directoryContext ) {
+		List<String> commands = new ArrayList<String>();
+		int result = 0;
+		File folder = null;
+		
+		if ( (directoryContext != null) && (!directoryContext.equals("")) ) {
+			folder = new File( directoryContext );
+		}
+		
+	    commands.add("/bin/sh");
+	    commands.add("-c");
+	    commands.add(command);
+		
+	    try {
+			SystemCommandExecutor commandExecutor = new SystemCommandExecutor(commands);
+			result = commandExecutor.executeCommand( folder );
+	    } catch ( Exception e ) {
+	    	result = 1;
+	    }
+		return result;
+	}
+	
 	
 	public static void run( String application ) {
+		
+		System.out.println(" run(): " + application );
+		
+		
 		Process process = null;
         try {
         	process = Runtime.getRuntime().exec( application );
@@ -108,41 +156,6 @@ public class Main {
         }
     }
 	
-	
-	public static void generateParameter(String adjacency, String laplacian, String slaplacian, String optiFunc, String caixa1, String ordermin,
-			String ordermax, String minDegree, String maxDegree, String triangleFree, String allowDiscGraphs, String biptOnly) {
-
-			String SEPARATOR_SYMBOL = ";";
-		
-			int minimumOrder = Integer.valueOf(ordermin);
-			int maximumOrder = Integer.valueOf(ordermax);
-			
-			for (int order = minimumOrder; order < maximumOrder; order++) {
-
-				String graphOptions = "";
-				if ( biptOnly.equals("on") ) {
-					graphOptions += "-b ";
-				}
-				if ( triangleFree.equals("on") ) {
-					graphOptions += "-t ";
-				}
-				if ( !allowDiscGraphs.equals("on") ) {
-					graphOptions += "-c ";
-				}
-
-				String degreeOptions = "";
-				degreeOptions += " -d" + minDegree;
-				degreeOptions += " -D" + maxDegree;
-
-				String optimizationType = ( caixa1.equals("max") ? "maximization" : "minimization");				
-				
-				System.out.println( order + SEPARATOR_SYMBOL + optiFunc + SEPARATOR_SYMBOL
-						+ optimizationType + SEPARATOR_SYMBOL + graphOptions
-						+ SEPARATOR_SYMBOL + degreeOptions);
-			}
-
-	}
-
 	
 	/**
 	 * This is a method to read the CSV data 
