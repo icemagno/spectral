1ead9510e8a3dc06ced3d3e20b86e089123a7b2c
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2015-05-24 18:53:44 -0300

    Ajustes na documentação

--- wrappers/union.sql
+++
@@ -1,5 +0,0 @@
-insert into union_out 
-	(id_pipeline, id_experiment, id_activity, caixa1, gvfile, optifunc, evaluatedvalue, g6fileid) 
-select %ID_PIP%, %ID_EXP%, %ID_ACT%, graphviz_out.caixa1, graphviz_out.gvfile, evaluate_out.optifunc, evaluate_out.evaluatedvalue, 
-	evaluate_out.g6fileid from graphviz_out, evaluate_out 
-where evaluate_out.g6fileid = graphviz_out.g6fileid and graphviz_out.id_experiment = %ID_EXP%
---
+++ wrappers/union_executor.sql
@@ -0,0 +1,5 @@
+insert into union_out 
+	(id_pipeline, id_experiment, id_activity, caixa1, gvfile, optifunc, evaluatedvalue, g6fileid, maxresults) 
+select %ID_PIP%, %ID_EXP%, %ID_ACT%, graphviz_out.caixa1, graphviz_out.gvfile, evaluate_out.optifunc, evaluate_out.evaluatedvalue, 
+	evaluate_out.g6fileid, evaluate_out.maxresults from graphviz_out, evaluate_out 
+where evaluate_out.g6fileid = graphviz_out.g6fileid and graphviz_out.id_experiment = %ID_EXP%
